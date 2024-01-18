6368a7f34416568cf496c41824b346b9558075ec
Kai <krikhye@gmail.com>
2024-01-18 16:18:19 -0600

    fix: add gcloud auth header for token rotation call

--- setup.py
+++ setup.py
@@ -25,7 +25,8 @@ setup(name='tap-quickbooks',
           'xmltodict==0.11.0',
           'jsonpath-ng==1.4.3',
           'pytz==2018.4',
-          'attrs==20.2.0'
+          'attrs==20.2.0',
+          'google-api-python-client==2.113.0',
       ],
       entry_points='''
           [console_scripts]
